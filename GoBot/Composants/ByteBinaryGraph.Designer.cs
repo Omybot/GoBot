19f77e2e9ef532746aa7fe1ad0422d7a06829c37
CF <christopher.forgeron@microvision.fr>
2017-10-10 17:32:13 +0200

    Création de ByteBinaryGraph

---
+++ GoBot/Composants/ByteBinaryGraph.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Composants
+{
+    partial class ByteBinaryGraph
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.binaryGraph1 = new Composants.BinaryGraph();
+            this.binaryGraph2 = new Composants.BinaryGraph();
+            this.binaryGraph3 = new Composants.BinaryGraph();
+            this.binaryGraph4 = new Composants.BinaryGraph();
+            this.binaryGraph5 = new Composants.BinaryGraph();
+            this.binaryGraph6 = new Composants.BinaryGraph();
+            this.binaryGraph7 = new Composants.BinaryGraph();
+            this.binaryGraph8 = new Composants.BinaryGraph();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // binaryGraph1
+            // 
+            this.binaryGraph1.BackColor = System.Drawing.Color.Transparent;
+            this.binaryGraph1.Location = new System.Drawing.Point(45, -1);
+            this.binaryGraph1.Name = "binaryGraph1";
+            this.binaryGraph1.Size = new System.Drawing.Size(214, 27);
+            this.binaryGraph1.TabIndex = 0;
+            // 
+            // binaryGraph2
+            // 
+            this.binaryGraph2.BackColor = System.Drawing.Color.Transparent;
+            this.binaryGraph2.Location = new System.Drawing.Point(45, 25);
+            this.binaryGraph2.Name = "binaryGraph2";
+            this.binaryGraph2.Size = new System.Drawing.Size(214, 27);
+            this.binaryGraph2.TabIndex = 1;
+            // 
+            // binaryGraph3
+            // 
+            this.binaryGraph3.BackColor = System.Drawing.Color.Transparent;
+            this.binaryGraph3.Location = new System.Drawing.Point(45, 51);
+            this.binaryGraph3.Name = "binaryGraph3";
+            this.binaryGraph3.Size = new System.Drawing.Size(214, 27);
+            this.binaryGraph3.TabIndex = 2;
+            // 
+            // binaryGraph4
+            // 
+            this.binaryGraph4.BackColor = System.Drawing.Color.Transparent;
+            this.binaryGraph4.Location = new System.Drawing.Point(45, 77);
+            this.binaryGraph4.Name = "binaryGraph4";
+            this.binaryGraph4.Size = new System.Drawing.Size(214, 27);
+            this.binaryGraph4.TabIndex = 3;
+            // 
+            // binaryGraph5
+            // 
+            this.binaryGraph5.BackColor = System.Drawing.Color.Transparent;
+            this.binaryGraph5.Location = new System.Drawing.Point(45, 103);
+            this.binaryGraph5.Name = "binaryGraph5";
+            this.binaryGraph5.Size = new System.Drawing.Size(214, 27);
+            this.binaryGraph5.TabIndex = 4;
+            // 
+            // binaryGraph6
+            // 
+            this.binaryGraph6.BackColor = System.Drawing.Color.Transparent;
+            this.binaryGraph6.Location = new System.Drawing.Point(45, 129);
+            this.binaryGraph6.Name = "binaryGraph6";
+            this.binaryGraph6.Size = new System.Drawing.Size(214, 27);
+            this.binaryGraph6.TabIndex = 5;
+            // 
+            // binaryGraph7
+            // 
+            this.binaryGraph7.BackColor = System.Drawing.Color.Transparent;
+            this.binaryGraph7.Location = new System.Drawing.Point(45, 155);
+            this.binaryGraph7.Name = "binaryGraph7";
+            this.binaryGraph7.Size = new System.Drawing.Size(214, 27);
+            this.binaryGraph7.TabIndex = 6;
+            // 
+            // binaryGraph8
+            // 
+            this.binaryGraph8.BackColor = System.Drawing.Color.Transparent;
+            this.binaryGraph8.Location = new System.Drawing.Point(45, 181);
+            this.binaryGraph8.Name = "binaryGraph8";
+            this.binaryGraph8.Size = new System.Drawing.Size(214, 27);
+            this.binaryGraph8.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 26);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "-";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(0, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 26);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "-";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(0, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 26);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "-";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(0, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 26);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "-";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(0, 104);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 26);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "-";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Location = new System.Drawing.Point(0, 130);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 26);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "-";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Location = new System.Drawing.Point(0, 156);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 26);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "-";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Location = new System.Drawing.Point(0, 182);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 26);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "-";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ByteBinaryGraph
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.binaryGraph8);
+            this.Controls.Add(this.binaryGraph7);
+            this.Controls.Add(this.binaryGraph6);
+            this.Controls.Add(this.binaryGraph5);
+            this.Controls.Add(this.binaryGraph4);
+            this.Controls.Add(this.binaryGraph3);
+            this.Controls.Add(this.binaryGraph2);
+            this.Controls.Add(this.binaryGraph1);
+            this.Name = "ByteBinaryGraph";
+            this.Size = new System.Drawing.Size(259, 208);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private BinaryGraph binaryGraph1;
+        private BinaryGraph binaryGraph2;
+        private BinaryGraph binaryGraph3;
+        private BinaryGraph binaryGraph4;
+        private BinaryGraph binaryGraph5;
+        private BinaryGraph binaryGraph6;
+        private BinaryGraph binaryGraph7;
+        private BinaryGraph binaryGraph8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
---
+++ GoBot/Composants/ByteBinaryGraph.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Composants
+{
+    public partial class ByteBinaryGraph : UserControl
+    {
+        private List<Label> labels;
+        private List<BinaryGraph> graphs;
+
+        public ByteBinaryGraph()
+        {
+            InitializeComponent();
+
+            labels = new List<Label>() { label1, label2, label3, label4, label5, label6, label7, label8 };
+            graphs = new List<BinaryGraph>() { binaryGraph1, binaryGraph2, binaryGraph3, binaryGraph4, binaryGraph5, binaryGraph6, binaryGraph7, binaryGraph8 };
+        }
+
+        public void SetNames(List<String> names)
+        {
+            for (int i = 0; i < names.Count && i < labels.Count; i++)
+            {
+                labels[i].Text = names[i];
+            }
+        }
+
+        public void SetValue(Byte value)
+        {
+            for(int i = 0; i < graphs.Count; i++)
+            {
+                graphs[i].AddPoint((value & 0x1) > 0);
+                value = (Byte)(value >> 1);
+            }
+        }
+    }
+}
