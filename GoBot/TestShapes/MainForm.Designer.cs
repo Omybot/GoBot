8950e7820caa7be4ed3f947ae3546c7de342de45
Forgeron Christopher <forgeron.christopher@gmail.com>
2018-06-14 23:18:40 +0200

    Tracé à la main de formes dans l'appli de test

--- GoBot/TestShapes/MainForm.Designer.cs
+++ GoBot/TestShapes/MainForm.Designer.cs
@@ -38,6 +38,13 @@
             this.btnCrossingPoints = new Composants.SwitchButton();
             this.btnCrossing = new Composants.SwitchButton();
             this.btnBarycenter = new Composants.SwitchButton();
+            this.btnRectangle = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnGrid = new Composants.SwitchButton();
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.btnSegment = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnCircleFromCenter = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picWorld)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,11 +60,14 @@
             this.picWorld.TabIndex = 1;
             this.picWorld.TabStop = false;
             this.picWorld.Paint += new System.Windows.Forms.PaintEventHandler(this.picWorld_Paint);
+            this.picWorld.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picWorld_MouseDown);
+            this.picWorld.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picWorld_MouseMove);
+            this.picWorld.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picWorld_MouseUp);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(51, 38);
+            this.label1.Location = new System.Drawing.Point(51, 59);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(63, 13);
             this.label1.TabIndex = 6;
@@ -66,7 +76,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(51, 63);
+            this.label2.Location = new System.Drawing.Point(51, 84);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(83, 13);
             this.label2.TabIndex = 7;
@@ -75,7 +85,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(51, 88);
+            this.label3.Location = new System.Drawing.Point(51, 109);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(105, 13);
             this.label3.TabIndex = 8;
@@ -84,7 +94,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(51, 113);
+            this.label4.Location = new System.Drawing.Point(51, 134);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(94, 13);
             this.label4.TabIndex = 9;
@@ -94,13 +104,13 @@
             // 
             this.btnContained.AutoSize = true;
             this.btnContained.BackColor = System.Drawing.Color.Transparent;
-            this.btnContained.Location = new System.Drawing.Point(10, 113);
+            this.btnContained.Location = new System.Drawing.Point(10, 134);
             this.btnContained.MaximumSize = new System.Drawing.Size(35, 15);
             this.btnContained.MinimumSize = new System.Drawing.Size(35, 15);
             this.btnContained.Mirrored = false;
             this.btnContained.Name = "btnContained";
             this.btnContained.Size = new System.Drawing.Size(35, 15);
-            this.btnContained.TabIndex = 5;
+            this.btnContained.TabIndex = 4;
             this.btnContained.Value = true;
             this.btnContained.ValueChanged += new Composants.SwitchButton.ValueChangedDelegate(this.btnContained_ValueChanged);
             // 
@@ -108,13 +118,13 @@
             // 
             this.btnCrossingPoints.AutoSize = true;
             this.btnCrossingPoints.BackColor = System.Drawing.Color.Transparent;
-            this.btnCrossingPoints.Location = new System.Drawing.Point(10, 88);
+            this.btnCrossingPoints.Location = new System.Drawing.Point(10, 109);
             this.btnCrossingPoints.MaximumSize = new System.Drawing.Size(35, 15);
             this.btnCrossingPoints.MinimumSize = new System.Drawing.Size(35, 15);
             this.btnCrossingPoints.Mirrored = false;
             this.btnCrossingPoints.Name = "btnCrossingPoints";
             this.btnCrossingPoints.Size = new System.Drawing.Size(35, 15);
-            this.btnCrossingPoints.TabIndex = 4;
+            this.btnCrossingPoints.TabIndex = 3;
             this.btnCrossingPoints.Value = true;
             this.btnCrossingPoints.ValueChanged += new Composants.SwitchButton.ValueChangedDelegate(this.btnCrossingPoints_ValueChanged);
             // 
@@ -122,13 +132,13 @@
             // 
             this.btnCrossing.AutoSize = true;
             this.btnCrossing.BackColor = System.Drawing.Color.Transparent;
-            this.btnCrossing.Location = new System.Drawing.Point(10, 63);
+            this.btnCrossing.Location = new System.Drawing.Point(10, 84);
             this.btnCrossing.MaximumSize = new System.Drawing.Size(35, 15);
             this.btnCrossing.MinimumSize = new System.Drawing.Size(35, 15);
             this.btnCrossing.Mirrored = false;
             this.btnCrossing.Name = "btnCrossing";
             this.btnCrossing.Size = new System.Drawing.Size(35, 15);
-            this.btnCrossing.TabIndex = 3;
+            this.btnCrossing.TabIndex = 2;
             this.btnCrossing.Value = true;
             this.btnCrossing.ValueChanged += new Composants.SwitchButton.ValueChangedDelegate(this.btnCrossing_ValueChanged);
             // 
@@ -136,21 +146,100 @@
             // 
             this.btnBarycenter.AutoSize = true;
             this.btnBarycenter.BackColor = System.Drawing.Color.Transparent;
-            this.btnBarycenter.Location = new System.Drawing.Point(10, 38);
+            this.btnBarycenter.Location = new System.Drawing.Point(10, 59);
             this.btnBarycenter.MaximumSize = new System.Drawing.Size(35, 15);
             this.btnBarycenter.MinimumSize = new System.Drawing.Size(35, 15);
             this.btnBarycenter.Mirrored = false;
             this.btnBarycenter.Name = "btnBarycenter";
             this.btnBarycenter.Size = new System.Drawing.Size(35, 15);
-            this.btnBarycenter.TabIndex = 2;
+            this.btnBarycenter.TabIndex = 1;
             this.btnBarycenter.Value = true;
             this.btnBarycenter.ValueChanged += new Composants.SwitchButton.ValueChangedDelegate(this.btnBarycenter_ValueChanged);
+            // 
+            // btnRectangle
+            // 
+            this.btnRectangle.Image = global::TestShapes.Properties.Resources.Rectangle;
+            this.btnRectangle.Location = new System.Drawing.Point(12, 198);
+            this.btnRectangle.Name = "btnRectangle";
+            this.btnRectangle.Size = new System.Drawing.Size(40, 40);
+            this.btnRectangle.TabIndex = 10;
+            this.btnRectangle.UseVisualStyleBackColor = true;
+            this.btnRectangle.Click += new System.EventHandler(this.btnRectangle_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(51, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Grille";
+            // 
+            // btnGrid
+            // 
+            this.btnGrid.AutoSize = true;
+            this.btnGrid.BackColor = System.Drawing.Color.Transparent;
+            this.btnGrid.Location = new System.Drawing.Point(10, 33);
+            this.btnGrid.MaximumSize = new System.Drawing.Size(35, 15);
+            this.btnGrid.MinimumSize = new System.Drawing.Size(35, 15);
+            this.btnGrid.Mirrored = false;
+            this.btnGrid.Name = "btnGrid";
+            this.btnGrid.Size = new System.Drawing.Size(35, 15);
+            this.btnGrid.TabIndex = 0;
+            this.btnGrid.Value = true;
+            this.btnGrid.ValueChanged += new Composants.SwitchButton.ValueChangedDelegate(this.btnGrid_ValueChanged);
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.Image = global::TestShapes.Properties.Resources.Circle1;
+            this.btnCircle.Location = new System.Drawing.Point(12, 244);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(40, 40);
+            this.btnCircle.TabIndex = 13;
+            this.btnCircle.UseVisualStyleBackColor = true;
+            this.btnCircle.Click += new System.EventHandler(this.btnCircle_Click);
+            // 
+            // btnSegment
+            // 
+            this.btnSegment.Image = global::TestShapes.Properties.Resources.Segment;
+            this.btnSegment.Location = new System.Drawing.Point(12, 290);
+            this.btnSegment.Name = "btnSegment";
+            this.btnSegment.Size = new System.Drawing.Size(40, 40);
+            this.btnSegment.TabIndex = 14;
+            this.btnSegment.UseVisualStyleBackColor = true;
+            this.btnSegment.Click += new System.EventHandler(this.btnSegment_Click);
+            // 
+            // button3
+            // 
+            this.button3.Image = global::TestShapes.Properties.Resources.Line;
+            this.button3.Location = new System.Drawing.Point(12, 336);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(40, 40);
+            this.button3.TabIndex = 15;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnCircleFromCenter
+            // 
+            this.btnCircleFromCenter.Image = global::TestShapes.Properties.Resources.Circle2;
+            this.btnCircleFromCenter.Location = new System.Drawing.Point(58, 244);
+            this.btnCircleFromCenter.Name = "btnCircleFromCenter";
+            this.btnCircleFromCenter.Size = new System.Drawing.Size(40, 40);
+            this.btnCircleFromCenter.TabIndex = 16;
+            this.btnCircleFromCenter.UseVisualStyleBackColor = true;
+            this.btnCircleFromCenter.Click += new System.EventHandler(this.btnCircleFromCenter_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(735, 683);
+            this.Controls.Add(this.btnCircleFromCenter);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnSegment);
+            this.Controls.Add(this.btnCircle);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnGrid);
+            this.Controls.Add(this.btnRectangle);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -179,6 +268,13 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRectangle;
+        private System.Windows.Forms.Label label5;
+        private Composants.SwitchButton btnGrid;
+        private System.Windows.Forms.Button btnCircle;
+        private System.Windows.Forms.Button btnSegment;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnCircleFromCenter;
     }
 }
 
--- GoBot/TestShapes/MainForm.cs
+++ GoBot/TestShapes/MainForm.cs
@@ -20,6 +20,7 @@ namespace TestShapes
         private bool _crossingPointsVisible = true;
         private bool _crossingColorVisible = true;
         private bool _containedColorVisible = true;
+        private bool _gridVisible = true;
 
         public MainForm()
         {
@@ -34,7 +35,7 @@ namespace TestShapes
             _shapes.Add(new Circle(new RealPoint(260, 500), 30));
             _shapes.Add(new PolygonRectangle(new RealPoint(230, 400), 300, 40));
             _shapes.Add(new Circle(new RealPoint(320, 420), 15));
-            
+
             _shapes.Add(new Circle(new RealPoint(100, 200), 15));
             _shapes.Add(new Circle(new RealPoint(100, 200), 30));
             _shapes.Add(new Circle(new RealPoint(100, 200), 45));
@@ -59,14 +60,19 @@ namespace TestShapes
 
         private void picWorld_Paint(object sender, PaintEventArgs e)
         {
+            DrawGrid(e.Graphics);
+
             e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
 
-            foreach (IShape shape in _shapes)
+            List<IShape> allShapes = new List<IShape>(_shapes);
+            if (_currentShape != null) allShapes.Add(_currentShape);
+
+            foreach (IShape shape in allShapes)
             {
                 bool isContained = false;
                 bool isCrossed = false;
 
-                foreach (IShape otherShape in _shapes)
+                foreach (IShape otherShape in allShapes)
                 {
                     if (otherShape != shape)
                     {
@@ -122,6 +128,24 @@ namespace TestShapes
             shape.Paint(g, outlineColor, 1, fillColor, WorldScale.Default());
         }
 
+        private void DrawGrid(Graphics g)
+        {
+            if (_gridVisible)
+            {
+                for (int i = 0; i < picWorld.Width; i += 10)
+                    g.DrawLine(Pens.WhiteSmoke, i, 0, i, picWorld.Height);
+
+                for (int i = 0; i < picWorld.Height; i += 10)
+                    g.DrawLine(Pens.WhiteSmoke, 0, i, picWorld.Width, i);
+
+                for (int i = 0; i < picWorld.Width; i += 100)
+                    g.DrawLine(Pens.LightGray, i, 0, i, picWorld.Height);
+
+                for (int i = 0; i < picWorld.Height; i += 100)
+                    g.DrawLine(Pens.LightGray, 0, i, picWorld.Width, i);
+            }
+        }
+
         #endregion
 
         #region Actions IHM
@@ -150,6 +174,118 @@ namespace TestShapes
             picWorld.Invalidate();
         }
 
+        private void btnGrid_ValueChanged(object sender, bool value)
+        {
+            _gridVisible = value;
+            picWorld.Invalidate();
+        }
+
         #endregion
+
+        private int _shapeMode;
+        private RealPoint _startPoint;
+        private IShape _currentShape;
+
+        private void btnRectangle_Click(object sender, EventArgs e)
+        {
+            _shapeMode = 1;
+            picWorld.Cursor = Cursors.Cross;
+        }
+
+        private void picWorld_MouseDown(object sender, MouseEventArgs e)
+        {
+            _startPoint = picWorld.PointToClient(Cursor.Position);
+
+            if (_shapeMode == 1)
+            {
+                _currentShape = new PolygonRectangle(_startPoint, 0, 0);
+            }
+            else if (_shapeMode == 2)
+            {
+                _currentShape = new Circle(_startPoint, 0);
+            }
+            else if (_shapeMode == 3)
+            {
+                _currentShape = new Circle(_startPoint, 0);
+            }
+            else if (_shapeMode == 4)
+            {
+                _currentShape = new Segment(_startPoint, _startPoint);
+            }
+        }
+
+        private void picWorld_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_startPoint != null)
+            {
+                RealPoint currentPoint = picWorld.PointToClient(Cursor.Position);
+
+                if (_shapeMode == 1)
+                {
+                    _currentShape = new PolygonRectangle(_startPoint, currentPoint.X - _startPoint.X, currentPoint.Y - _startPoint.Y);
+                }
+                else if (_shapeMode == 2)
+                {
+                    _currentShape = new Circle(new Segment(_startPoint, currentPoint).Barycenter, _startPoint.Distance(currentPoint) / 2);
+                }
+                else if (_shapeMode == 3)
+                {
+                    _currentShape = new Circle(_startPoint, _startPoint.Distance(currentPoint));
+                }
+                else if (_shapeMode == 4)
+                {
+                    _currentShape = new Segment(_startPoint, currentPoint);
+                }
+
+                picWorld.Invalidate();
+            }
+        }
+
+        private void picWorld_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (_startPoint != null)
+            {
+                RealPoint currentPoint = picWorld.PointToClient(Cursor.Position);
+                _currentShape = null;
+
+                if (_shapeMode == 1)
+                {
+                    _shapes.Add(new PolygonRectangle(_startPoint, currentPoint.X - _startPoint.X, currentPoint.Y - _startPoint.Y));
+                }
+                else if (_shapeMode == 2)
+                {
+                    _shapes.Add(new Circle(new Segment(_startPoint, currentPoint).Barycenter, _startPoint.Distance(currentPoint) / 2));
+                }
+                else if (_shapeMode == 3)
+                {
+                    _shapes.Add(new Circle(_startPoint, _startPoint.Distance(currentPoint)));
+                }
+                else if (_shapeMode == 4)
+                {
+                    _shapes.Add(new Segment(_startPoint, currentPoint));
+                }
+
+                picWorld.Invalidate();
+                _startPoint = null;
+            }
+        }
+
+        private void btnCircle_Click(object sender, EventArgs e)
+        {
+            _shapeMode = 2;
+            picWorld.Cursor = Cursors.Cross;
+        }
+
+        private void btnCircleFromCenter_Click(object sender, EventArgs e)
+        {
+            _shapeMode = 3;
+            picWorld.Cursor = Cursors.Cross;
+        }
+
+        private void btnSegment_Click(object sender, EventArgs e)
+        {
+            _shapeMode = 4;
+            picWorld.Cursor = Cursors.Cross;
+        }
     }
 }
