5197ad85d1d8a1b0440815c4daa480ccf8030f4d
Forgeron Christopher <forgeron.christopher@gmail.com>
2018-09-26 23:41:02 +0200

    Ajout des tests de distance et de points de croisements entre cercles

--- GoBot/Geometry/Shapes/ShapesInteractions/CircleWithCircle.cs
+++ GoBot/Geometry/Shapes/ShapesInteractions/CircleWithCircle.cs
@@ -45,39 +45,51 @@ namespace Geometry.Shapes.ShapesInteractions
 
             List<RealPoint> output = new List<RealPoint>();
 
-            bool aligned = Math.Abs(circle1.Center.Y - circle2.Center.Y) < RealPoint.PRECISION;
-
-            if (aligned)// Cercles non alignés horizontalement (on pivote pour les calculs, sinon division par 0)
-                circle1 = circle1.Rotation(90, circle2.Center);
-
-            RealPoint oc1 = new RealPoint(circle1.Center), oc2 = new RealPoint(circle2.Center);
-            double b = circle1.Radius, c = circle2.Radius;
-
-            double a = (-(Math.Pow(oc1.X, 2)) - (Math.Pow(oc1.Y, 2)) + Math.Pow(oc2.X, 2) + Math.Pow(oc2.Y, 2) + Math.Pow(b, 2) - Math.Pow(c, 2)) / (2 * (oc2.Y - oc1.Y));
-            double d = ((oc2.X - oc1.X) / (oc2.Y - oc1.Y));
-
-            double A = Math.Pow(d, 2) + 1;
-            double B = -2 * oc1.X + 2 * oc1.Y * d - 2 * a * d;
-            double C = Math.Pow(oc1.X, 2) + Math.Pow(oc1.Y, 2) - 2 * oc1.Y * a + Math.Pow(a, 2) - Math.Pow(b, 2);
-
-            double delta = Math.Pow(B, 2) - 4 * A * C;
-
-            if (delta >= 0)
+            if (circle1.Radius == 0 && circle2.Radius == 0 && circle1.Center == circle2.Center)
             {
-                double x1 = (-B + Math.Sqrt(delta)) / (2 * A);
-                double y1 = a - x1 * d;
-                output.Add(new RealPoint(x1, y1));
-
-                if (delta > 0)
-                {
-                    double x2 = (-B - Math.Sqrt(delta)) / (2 * A);
-                    double y2 = a - x2 * d;
-                    output.Add(new RealPoint(x2, y2));
-                }
+                // Cas particulier où les deux cercles ont un rayon de 0 et sont au même endroit : le croisement c'est ce point.
+                output.Add(new RealPoint(circle1.Center));
             }
+            else if (circle1.Center == circle2.Center)
+            {
+                // Cas particulier où les deux cercles ont un rayon > 0 et sont au même endroit : on ne calcule pas les points de croisement, même s'ils se superposent (une autre idée est la bienvenue ?)
+            }
+            else
+            {
+                bool aligned = Math.Abs(circle1.Center.Y - circle2.Center.Y) < RealPoint.PRECISION;
 
-            if (aligned)
-                output = output.ConvertAll(p => p.Rotation(-90, circle2.Center));
+                if (aligned)// Cercles non alignés horizontalement (on pivote pour les calculs, sinon division par 0)
+                    circle1 = circle1.Rotation(90, circle2.Center);
+
+                RealPoint oc1 = new RealPoint(circle1.Center), oc2 = new RealPoint(circle2.Center);
+                double b = circle1.Radius, c = circle2.Radius;
+
+                double a = (-(Math.Pow(oc1.X, 2)) - (Math.Pow(oc1.Y, 2)) + Math.Pow(oc2.X, 2) + Math.Pow(oc2.Y, 2) + Math.Pow(b, 2) - Math.Pow(c, 2)) / (2 * (oc2.Y - oc1.Y));
+                double d = ((oc2.X - oc1.X) / (oc2.Y - oc1.Y));
+
+                double A = Math.Pow(d, 2) + 1;
+                double B = -2 * oc1.X + 2 * oc1.Y * d - 2 * a * d;
+                double C = Math.Pow(oc1.X, 2) + Math.Pow(oc1.Y, 2) - 2 * oc1.Y * a + Math.Pow(a, 2) - Math.Pow(b, 2);
+
+                double delta = Math.Pow(B, 2) - 4 * A * C;
+
+                if (delta >= 0)
+                {
+                    double x1 = (-B + Math.Sqrt(delta)) / (2 * A);
+                    double y1 = a - x1 * d;
+                    output.Add(new RealPoint(x1, y1));
+
+                    if (delta > 0)
+                    {
+                        double x2 = (-B - Math.Sqrt(delta)) / (2 * A);
+                        double y2 = a - x2 * d;
+                        output.Add(new RealPoint(x2, y2));
+                    }
+                }
+
+                if (aligned)
+                    output = output.ConvertAll(p => p.Rotation(-90, circle2.Center));
+            }
 
             return output;
         }
--- GoBot/GeometryTester/TestCircleWithCircle.cs
+++ GoBot/GeometryTester/TestCircleWithCircle.cs
@@ -272,5 +272,234 @@ namespace GeometryTester
 
 
         #endregion
+
+        #region Distance
+
+        [TestMethod]
+        public void TestDistanceStandard()
+        {
+            Circle c1 = new Circle(new RealPoint(3, 4), 1);
+            Circle c2 = new Circle(new RealPoint(6, 10), 1);
+
+            Assert.AreEqual(4.71, c1.Distance(c2), RealPoint.PRECISION);
+        }
+
+        [TestMethod]
+        public void TestDistanceSame()
+        {
+            Circle c1 = new Circle(new RealPoint(20, 30), 20);
+            Circle c2 = new Circle(new RealPoint(20, 30), 20);
+
+            Assert.AreEqual(0, c1.Distance(c2), RealPoint.PRECISION);
+        }
+
+        [TestMethod]
+        public void TestDistanceAdjacent()
+        {
+            // 3² + 4² = 5², très pratique
+
+            Circle c1 = new Circle(new RealPoint(10, 10), 2.5);
+            Circle c2 = new Circle(new RealPoint(13, 14), 2.5);
+
+            Assert.AreEqual(0, c1.Distance(c2), RealPoint.PRECISION);
+        }
+
+        [TestMethod]
+        public void TestDistanceRadiusZeroBoth()
+        {
+            Circle c1 = new Circle(new RealPoint(10, 10), 0);
+            Circle c2 = new Circle(new RealPoint(10, 10), 0);
+
+            Assert.AreEqual(0, c1.Distance(c2), RealPoint.PRECISION);
+        }
+
+        [TestMethod]
+        public void TestDistanceRadiusZeroOnCircle()
+        {
+            // 3² + 4² = 5², très pratique
+
+            Circle c1 = new Circle(new RealPoint(10, 10), 5);
+            Circle c2 = new Circle(new RealPoint(13, 14), 0);
+
+            Assert.AreEqual(0, c1.Distance(c2), RealPoint.PRECISION);
+        }
+
+        [TestMethod]
+        public void TestDistanceRadiusZeroOnCenter()
+        {
+            // 3² + 4² = 5², très pratique
+
+            Circle c1 = new Circle(new RealPoint(3, 4), 5);
+            Circle c2 = new Circle(new RealPoint(0, 0), 0);
+
+            Assert.AreEqual(0, c1.Distance(c2), RealPoint.PRECISION);
+        }
+
+        [TestMethod]
+        public void TestDistanceCrossing()
+        {
+            Circle c1 = new Circle(new RealPoint(5, 6), 5);
+            Circle c2 = new Circle(new RealPoint(6, 8), 5);
+
+            Assert.AreEqual(0, c1.Distance(c2), RealPoint.PRECISION);
+        }
+
+        [TestMethod]
+        public void TestDistanceCenterNeg()
+        {
+            Circle c1 = new Circle(new RealPoint(-20, -30), 5);
+            Circle c2 = new Circle(new RealPoint(-55, -35), 4);
+
+            Assert.AreEqual(26.35, c1.Distance(c2), RealPoint.PRECISION);
+        }
+
+        #endregion
+
+        #region CrossingPoints
+
+        [TestMethod]
+        public void TestCrossingPointsStandard()
+        {
+            Circle c1 = new Circle(new RealPoint(20, 30), 50);
+            Circle c2 = new Circle(new RealPoint(55, 35), 40);
+
+            List<RealPoint> pts = c1.GetCrossingPoints(c2);
+
+            Assert.AreEqual(2, pts.Count);
+            Assert.IsTrue(pts.Contains(new RealPoint(55.71, -4.99)));
+            Assert.IsTrue(pts.Contains(new RealPoint(44.49, 73.59)));
+        }
+
+        [TestMethod]
+        public void TestCrossingPointsSameX()
+        {
+            Circle c1 = new Circle(new RealPoint(20, 30), 20);
+            Circle c2 = new Circle(new RealPoint(20, 35), 20);
+
+            List<RealPoint> pts = c1.GetCrossingPoints(c2);
+
+            Assert.AreEqual(2, pts.Count);
+            Assert.IsTrue(pts.Contains(new RealPoint(39.84, 32.5)));
+            Assert.IsTrue(pts.Contains(new RealPoint(0.16, 32.5)));
+        }
+
+        [TestMethod]
+        public void TestCrossingPointsSameY()
+        {
+            Circle c1 = new Circle(new RealPoint(20, 30), 20);
+            Circle c2 = new Circle(new RealPoint(35, 30), 15);
+
+            List<RealPoint> pts = c1.GetCrossingPoints(c2);
+
+            Assert.AreEqual(2, pts.Count);
+            Assert.IsTrue(pts.Contains(new RealPoint(33.33, 15.09)));
+            Assert.IsTrue(pts.Contains(new RealPoint(33.33, 44.91)));
+        }
+
+        [TestMethod]
+        public void TestCrossingPointsAdjacentX()
+        {
+            Circle c1 = new Circle(new RealPoint(20, 30), 20);
+            Circle c2 = new Circle(new RealPoint(60, 30), 20);
+
+            List<RealPoint> pts = c1.GetCrossingPoints(c2);
+
+            Assert.AreEqual(1, pts.Count);
+            Assert.IsTrue(pts.Contains(new RealPoint(40,30)));
+        }
+
+        [TestMethod]
+        public void TestCrossingPointsAdjacentY()
+        {
+            Circle c1 = new Circle(new RealPoint(30, 20), 20);
+            Circle c2 = new Circle(new RealPoint(30, 60), 20);
+
+            List<RealPoint> pts = c1.GetCrossingPoints(c2);
+
+            Assert.AreEqual(1, pts.Count);
+            Assert.IsTrue(pts.Contains(new RealPoint(30, 40)));
+        }
+
+        [TestMethod]
+        public void TestCrossingPointsAdjacent()
+        {
+            // 3² + 4² = 5², très pratique
+
+            Circle c1 = new Circle(new RealPoint(10, 10), 2.5);
+            Circle c2 = new Circle(new RealPoint(13, 14), 2.5);
+
+            List<RealPoint> pts = c1.GetCrossingPoints(c2);
+
+            Assert.AreEqual(1, pts.Count);
+            Assert.IsTrue(pts.Contains(new RealPoint(11.5,12)));
+        }
+
+        [TestMethod]
+        public void TestCrossingPointsRadiusZeroBoth()
+        {
+            Circle c1 = new Circle(new RealPoint(10, 10), 0);
+            Circle c2 = new Circle(new RealPoint(10, 10), 0);
+
+            List<RealPoint> pts = c1.GetCrossingPoints(c2);
+
+            Assert.AreEqual(1, pts.Count);
+            Assert.IsTrue(pts.Contains(new RealPoint(10,10)));
+        }
+
+        [TestMethod]
+        public void TestCrossingPointsRadiusZeroOnCircle()
+        {
+            // 3² + 4² = 5², très pratique
+
+            Circle c1 = new Circle(new RealPoint(10, 10), 5);
+            Circle c2 = new Circle(new RealPoint(13, 14), 0);
+
+            List<RealPoint> pts = c1.GetCrossingPoints(c2);
+
+            Assert.AreEqual(1, pts.Count);
+            Assert.IsTrue(pts.Contains(new RealPoint(13, 14)));
+        }
+
+        [TestMethod]
+        public void TestCrossingPointsRadiusZeroOnCenter()
+        {
+            // 3² + 4² = 5², très pratique
+
+            Circle c1 = new Circle(new RealPoint(3, 4), 5);
+            Circle c2 = new Circle(new RealPoint(0, 0), 0);
+
+            List<RealPoint> pts = c1.GetCrossingPoints(c2);
+
+            Assert.AreEqual(1, pts.Count);
+            Assert.IsTrue(pts.Contains(new RealPoint(0, 0)));
+        }
+
+        [TestMethod]
+        public void TestCrossingPointsSuperposed()
+        {
+            // Cas un peu particulier, les cercles se superposent don se croisent, mais on ne calcule pas de points de croisement car il y en une infinité
+
+            Circle c1 = new Circle(new RealPoint(5, 5), 5);
+            Circle c2 = new Circle(new RealPoint(5, 5), 5);
+
+            List<RealPoint> pts = c1.GetCrossingPoints(c2);
+
+            Assert.AreEqual(0, pts.Count);
+        }
+
+        [TestMethod]
+        public void TestCrossingPointsNeg()
+        {
+            Circle c1 = new Circle(new RealPoint(-20, -30), 50);
+            Circle c2 = new Circle(new RealPoint(-55, -35), 40);
+
+            List<RealPoint> pts = c1.GetCrossingPoints(c2);
+
+            Assert.AreEqual(2, pts.Count);
+            Assert.IsTrue(pts.Contains(new RealPoint(-44.49, -73.59)));
+            Assert.IsTrue(pts.Contains(new RealPoint(-55.71, 4.99)));
+        }
+
+        #endregion
     }
 }
