acd0edd1b0edd6646e6d73392096b7aaaf5a439c
Forgeron Christopher <forgeron.christopher@gmail.com>
2019-01-08 23:28:26 +0100

    Ajout LineWithCircle

---
+++ GoBot/Geometry/Shapes/ShapesInteractions/LineWithCircle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geometry.Shapes.ShapesInteractions
+{
+    public static class LineWithCircle
+    {
+        public static bool Contains(Line containingLine, Circle containedCircle)
+        {
+            // Une droite contient un cercle si son rayon est nul et qu'il est sur la droite
+
+            return containedCircle.Radius == 0 && containingLine.Contains(containedCircle.Center);
+        }
+    }
+}
